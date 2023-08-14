59ccc9f706c767a4d5d100e258e396c57402b4e3
Diego Rivera <diegosebsk@gmail.com>
2023-08-14 01:00:51 -0400

    change width to cart section

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,7 +4,7 @@ import { CartIcon, ClearCartIcon, RemoveFromCartIcon } from "./Icons";
 
 function CartItem({ thumbnail, price, title, quantity, addToCart }) {
   return (
-    <li className="border-b border-gray-600 pb-4">
+    <li className="border-b border-gray-600 pb-4 ">
       <img className="aspect-video w-full" src={thumbnail} alt={title} />
       <div>
         <span>{title}</span> - <span>${price}</span>
@@ -34,7 +34,7 @@ function Cart() {
         <CartIcon />
       </label>
       <input type="checkbox" id={cartCheckboxId} hidden className="peer" />
-      <aside className="translate-x-96 w-48 peer-checked:translate-x-0 h-full fixed top-0 right-0 p-8 bg-black/90 duration-500">
+      <aside className="translate-x-96 w-96 peer-checked:translate-x-0 h-full fixed top-0 right-0 p-8 bg-black/90 duration-500">
         <ul>
           {cart.map((product) => (
             <CartItem
